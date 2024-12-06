8a00d6b1706d3a4a4731c86a0fddf9834f12661f
seoyoungbaek <72129622+seoyoungbaek@users.noreply.github.com>
2024-12-05 20:24:51 -0800

    enemy board place fixed

--- Checkers/Battleship/Battleship/Battleship.cpp
+++ Checkers/Battleship/Battleship/Battleship.cpp
@@ -39,24 +39,24 @@ Battleship::~Battleship()       //destructor
 
 // Fire at tile
 void Battleship::fire(Tile* board[10][10], int x, int y) {
-    if (board[x][y]->shipexist()) {
-        board[x][y]->setText("X"); // Hit
-    }
-    else {
-        board[x][y]->setText("O"); // Miss
-    }
+	if (board[x][y]->shipexist()) {
+		board[x][y]->setText("X"); // Hit
+	}
+	else {
+		board[x][y]->setText("O"); // Miss
+	}
 }
 
 // Check game over
 bool Battleship::isGameOver(Tile* board[10][10]) {
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            if (board[i][j]->shipexist() && board[i][j]->text() != "X") {
-                return false;
-            }
-        }
-    }
-    return true;
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (board[i][j]->shipexist() && board[i][j]->text() != "X") {
+				return false;
+			}
+		}
+	}
+	return true;
 
 }
 
@@ -72,45 +72,62 @@ void Battleship::handleButton() {
 }
 
 void Battleship::placeEnemyShips() {
+	int shipLength[5] = { 5,4,3,3,2 };
 	for (int i = 0; i < 5; i++) {
 		Ship* ship = new Ship();
 		while (true) {
 			int x = rand() % 10;
 			int y = rand() % 10;
 			bool horizontal = (rand() % 2 == 0);
+			int length = shipLength[i];
 			bool isValid = true;
 			if (horizontal) {
-				if (x + 3 - 1 < 10) { // REPLACE 3 WITH SHIP LENGTH
-					for (int j = 0; j < 3; j++) {
+				if (x + length <= 10) {
+					for (int j = 0; j < length; j++) {
 						if (enemyBoard[x + j][y]->getShip() != nullptr) {
 							isValid = false;
+							break;
 						}
 					}
-					if (isValid) {
-						for (int j = 0; j < 3; j++) {
-							enemyBoard[x + j][y]->setText("S");
-							enemyBoard[x + j][y]->setShip(ship);
-						}
-						break;
-					}
+				}
+				else {
+					isValid = false;
 				}
 			}
 			else {
-				if (y + 3 - 1 < 10) { // REPLACE 3 WITH SHIP LENGTH
-					for (int j = 0; j < 3; j++) {
+				if (y + length <= 10) { //for vertical check
+					for (int j = 0; j < length; j++) {
 						if (enemyBoard[x][y + j]->getShip() != nullptr) {
 							isValid = false;
+							break;
 						}
-					}
-					if (isValid) {
-						for (int j = 0; j < 3; j++) {
-							enemyBoard[x][y + j]->setText("S");
-							enemyBoard[x][y + j]->setShip(ship);
-						}
-						break;
 					}
 				}
+				else {
+					isValid = false;
+				}
+			}
+			if (isValid) {
+				if (horizontal) {
+					for (int j = 0; j < length; j++) {
+						enemyBoard[x + j][y]->setText("S");
+						enemyBoard[x + j][y]->setShip(ship);
+					}
+				}
+				else {
+					for (int j = 0;j < length;j++) {
+						enemyBoard[x][y + j]->setText("S");
+						enemyBoard[x][y + j]->setShip(ship);
+					}
+				}
+				break;
 			}
 		}
 	}
+
+}
+
+
+void Battleship::placePlayerShipsManually() {
+
 }
--- Checkers/Battleship/Battleship/Battleship.h
+++ Checkers/Battleship/Battleship/Battleship.h
@@ -31,6 +31,6 @@ private:
 	void fire(Tile* board[10][10], int x, int y);	//fire opponent ship
 	bool isGameOver(Tile* board[10][10]);	//checks game over
 	void placeEnemyShips();
-
+	void placePlayerShipsManually();
 
 };
